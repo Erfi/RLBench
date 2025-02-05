b5929b45d31936f3516d981e71fe56fb7be7a054
Erfan Azad <erfan.basiri.azad@gmail.com>
2025-02-05 17:38:08 +0100

    new action mode for RL based on EEPose with planner

--- rlbench/action_modes/action_mode.py
+++ rlbench/action_modes/action_mode.py
@@ -2,8 +2,8 @@ from abc import abstractmethod
 
 import numpy as np
 
-from rlbench.action_modes.arm_action_modes import ArmActionMode, JointPosition
-from rlbench.action_modes.gripper_action_modes import GripperActionMode, GripperJointPosition
+from rlbench.action_modes.arm_action_modes import ArmActionMode, JointPosition, EndEffectorPoseViaPlanning
+from rlbench.action_modes.gripper_action_modes import GripperActionMode, GripperJointPosition, Discrete
 from rlbench.backend.scene import Scene
 
 
@@ -77,3 +77,39 @@ class JointPositionActionMode(ActionMode):
     def action_bounds(self):
         """Returns the min and max of the action mode."""
         return np.array(7 * [-0.1] + [0.0]), np.array(7 * [0.1] + [0.04])
+
+class EEPlannerActionMode(ActionMode):
+    """
+    An action mode that allows the end effector to be moved to a target pose
+    using the planner. The gripper is controlled using a discrete action space.
+    """
+    def __init__(self):
+        super(EEPlannerActionMode, self).__init__(
+            EndEffectorPoseViaPlanning(absolute_mode=True), Discrete())
+
+    def action(self, scene, action):
+        # create a valid quternion (qx, qy, qz, qw) from the action
+        arm_act_size = np.prod(self.arm_action_mode.action_shape(scene))
+        arm_action = np.array(action[:arm_act_size])
+        pos = arm_action[:3]
+        raw_quat = arm_action[3:]
+        quat = raw_quat / np.linalg.norm(raw_quat)
+
+        # apply arm action
+        arm_action = np.concatenate([pos, quat])
+        self.arm_action_mode.action(scene, arm_action)
+
+        # apply gripper action
+        ee_action = np.array(action[arm_act_size:])
+        self.gripper_action_mode.action(scene, ee_action)
+
+
+    def action_shape(self, scene):
+        # 3 position + 4 quaternion (qx, qy, qz, qw) + 1 gripper
+        return np.prod(self.arm_action_mode.action_shape(scene)) + np.prod(self.gripper_action_mode.action_shape(scene))
+    
+    def action_bounds(self):
+        """Returns the min and max of the action mode."""
+        low = np.array([-0.32, -0.45, 0.75] + 4*[-1.0] + [0.0])
+        high = np.array([0.32, 0.45, 1.75] + 4*[1.0] + [1.0])
+        return low, high
